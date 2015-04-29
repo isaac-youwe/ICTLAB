05725e031334a77d37ae938fd446d1e2652e764f
adrielstar <adri_0813548@hotmail.co.uk>
2015-04-29 16:42:22 +0200

    FUN-54 responsive map/ Correct google map

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -34,10 +34,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
         // JavaScript Files top of page
         $this->_view->headScript()->appendFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+        $this->_view->headScript()->appendFile('https://maps.googleapis.com/maps/api/js?v=3.exp');//Google Map API
         $this->_view->headScript()->appendFile('js/bootstrap.min.js');
         $this->_view->headScript()->appendFile('js/jquery.js');
         $this->_view->headScript()->appendFile('js/filter.js');
         $this->_view->headScript()->appendFile('js/jquery.sidr.min.js');
+        $this->_view->headScript()->appendFile('js/map.js');
     }
 }
 
