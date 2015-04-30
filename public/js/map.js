7dc4cb690760c7e08fc782c214b162ee3fef60bd
adrielstar <adri_star1986@hotmail.com>
2015-04-30 20:52:58 +0200

    Google-Map fix

--- public/js/map.js
+++ public/js/map.js
@@ -7,5 +7,4 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
