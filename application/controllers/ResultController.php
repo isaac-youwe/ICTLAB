13b66dc1061b29a1d61c583525368949a57f29f9
Isaac de Cuba <i.de.cuba@youwe.nl>
2015-05-05 17:45:57 +0200

    added new library geocoder and implemented it in the result controller

--- application/controllers/ResultController.php
+++ application/controllers/ResultController.php
@@ -1,17 +1,43 @@
 <?php
 
+/**
+ * Geocoder code and document available at:
+ * https://github.com/geocoder-php/Geocoder
+ * http://geocoder-php.org/Geocoder/
+ */
+
+use Ivory\HttpAdapter\CurlHttpAdapter;
+use Geocoder\Geocoder;
+use Geocoder\Model\Address;
+
 class ResultController extends Zend_Controller_Action
 {
+    private $_curl;
+    private $_geocoder;
 
     public function init()
     {
         /* Initialize action controller here */
+        $this->_curl = new CurlHttpAdapter();
+        $this->_geocoder = new \Geocoder\Provider\GoogleMaps($this->_curl);
     }
 
     public function indexAction()
     {
 //        $this->view->assign('baseUrl', Zend_Controller_Front::getBaseUrl() . Zend_Controller_Front::getInstance()->getRequest()->getRequestUri());
-        $this->view->assign('search', $this->getRequest()->getParam('search'));
+        $search = $this->getRequest()->getParam('search');
+        $this->view->assign('search', $search);
+
+        /**
+         * Geocoder\Model\Address $addresses
+         */
+        $addresses = $this->_geocoder->geocode($search);
+
+        foreach ($addresses as $address) {
+            $this->view->assign('lat', $address->getLatitude());
+            $this->view->assign('lng', $address->getLongitude());
+        }
+
         $this->view->fundaAanbod = new Application_Model_Funda_Aanbod();
     }
 
