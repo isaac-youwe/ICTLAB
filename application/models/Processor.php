d7fbe723a94a538a51c6660c915d28544c8bfe1a
ädrielstar¨ <adri_0813548@hotmail.co.uk>
2015-06-08 22:15:40 +0200

    dummy json

---
+++ application/models/Processor.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adriel
+ * Date: 8-6-15
+ * Time: 15:20
+ */
+
+class Application_Model_Processor {
+
+    /**
+     * @param json $input
+     * @return string
+     */
+    function getPolygonFromJson($content)
+    {
+        $json = json_decode($content);
+
+        foreach ($json as $object) {
+            $pol = "POLYGON((";
+            for ($i = 0; $i < count($object->polygon); $i++) {
+                $pol .= $object->polygon[$i][0] . " " . $object->polygon[$i][1] . ",";
+            }
+            $pol = substr($pol, 0, -1);
+            $pol .= "))";
+
+            return $pol;
+        }
+    }
+}
