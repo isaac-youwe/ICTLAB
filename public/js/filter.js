13b172c65fe45297103ab8a8cda389ae5473ed9b
flexi758 <serhildan_akdeniz@hotmail.com>
2015-06-10 20:12:51 +0200

    FUN-122

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,7 +1,15 @@
 /**
- * Created by Adriel on 4/24/2015.
+ * Created by Adriel Walter on 4/24/2015.
+ * submitForm by Serhildan Akdeniz.
  */
-
-$(document).ready(function() {
+$(document).ready(function () {
     $('#filter').sidr();
 });
+function submitForm() {
+    if (document.getElementById('searchid').value <= 2) {
+        //do nothing
+    }
+    else {
+        document.getElementById('form').submit();
+    }
+}
