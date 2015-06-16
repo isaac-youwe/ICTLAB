cce0994ab56362b1654eda6d26237a99530f22c9
Isaac de Cuba <i.de.cuba@youwe.nl>
2015-06-16 22:39:13 +0200

    bug fix and removed geocode from vendor

--- application/models/Google/Geocoding.php
+++ application/models/Google/Geocoding.php
@@ -8,7 +8,7 @@ use Guzzle\Http\Client;
 class Application_Model_Google_Geocoding
 {
     private $_guzzleClient;
-    public $baseUrl = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=';
+    public $baseUrl = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&';
 
     /**
      * Get the Geographic location of the search value
@@ -18,12 +18,25 @@ class Application_Model_Google_Geocoding
      */
     public function getLocation($search)
     {
-        $url = $this->baseUrl . $search;
+        $url = $this->baseUrl;
+        if (is_array($search)) {
+            foreach ($search as $value) {
+                $url .= 'address=' . $value . '&';
+            }
+            $url = substr($url, 0, -1);
+        } else {
+            $url .= 'address=' . $search;
+        }
+
         $request = $this->getGuzzleClient()->createRequest('GET', $url);
         $response = json_decode($request->send()->getBody(), true);
 
         if ($response['status'] = 'OK') {
-            return $response['results'][0]['geometry']['location'];
+            foreach ($response['results'] as $result) {
+                if (strpos($result['formatted_address'], 'Netherlands')) {
+                    return $response['results'][0]['geometry']['location'];
+                }
+            }
         } else {
             return false;
         }
