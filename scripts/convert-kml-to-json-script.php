80fe2e67600c7e0cfa8837ab3c78e69e11065ea5
Isaac de Cuba <i.de.cuba@youwe.nl>
2015-06-21 01:22:20 +0200

    Fix script

--- scripts/convert-kml-to-json-script.php
+++ scripts/convert-kml-to-json-script.php
@@ -56,7 +56,7 @@ function processFiles($path)
             "polygon" => filterCoordinates($xml->Document->Placemark->Polygon->outerBoundaryIs->LinearRing->coordinates));
 
         // convert string to json
-        $json = json_encode($stringToJson);
+        $json = safe_json_encode($stringToJson);
 //        echo $json . PHP_EOL;
 
         // create .json file into shapefiles folder
@@ -178,6 +178,42 @@ function filter($documentName, $type)
             echo 'Filter type is not correct!' . PHP_EOL;
             return '';
     }
+}
+
+function safe_json_encode($value){
+    if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+        $encoded = json_encode($value, JSON_PRETTY_PRINT);
+    } else {
+        $encoded = json_encode($value);
+    }
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            return $encoded;
+        case JSON_ERROR_DEPTH:
+            return 'Maximum stack depth exceeded'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_STATE_MISMATCH:
+            return 'Underflow or the modes mismatch'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_CTRL_CHAR:
+            return 'Unexpected control character found';
+        case JSON_ERROR_SYNTAX:
+            return 'Syntax error, malformed JSON'; // or trigger_error() or throw new Exception()
+        case JSON_ERROR_UTF8:
+            $clean = utf8ize($value);
+            return safe_json_encode($clean);
+        default:
+            return 'Unknown error'; // or trigger_error() or throw new Exception()
+    }
+}
+
+function utf8ize($mixed) {
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string ($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
 }
 
 echo PHP_EOL;
